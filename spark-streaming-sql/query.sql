f5b19a21543c7a519264bdbeadca787e5d8af27a
Chewing Glass <chewinglass123@gmail.com>
2023-06-24 10:13:28 -0500

    Fix query

--- spark-streaming-sql/query.sql
+++ spark-streaming-sql/query.sql
@@ -20,7 +20,7 @@ SELECT
     base64(poc_id) as poc_id,
     struct(
       from_epoch_millis(beacon_report.received_timestamp) as received_timestamp,
-      cast(beacon_report.location as decimal(23, 0)) location,
+      cast(beacon_report.location as decimal(23, 0)) location_h3,
       (beacon_report.hex_scale / 10000) as scale,
       b58encodeChecked(beacon_report.report.pub_key) as pub_key,
       base64(beacon_report.report.local_entropy) as local_entropy,
@@ -31,14 +31,14 @@ SELECT
       (beacon_report.report.tx_power / 10) as tx_power_dbm,    beacon_report.report.channel,
       beacon_report.report.frequency as frequency_hz,
       beacon_report.report.datarate,
-      beacon_report.report.signature,
+      base64(beacon_report.report.signature) as signature,
       (beacon_report.reward_unit / 10000) as reward_unit,
       (beacon_report.gain / 10) as gain_dbi,
-      beacon_report.elevation
+      beacon_report.elevation as elevation_agl_m
      ) as beacon,
      struct(
       from_epoch_millis(beacon_report.received_timestamp) as received_timestamp,
-      cast(witness.location as decimal(23, 0)) location,
+      cast(witness.location as decimal(23, 0)) location_h3,
       b58encodeChecked(witness.report.pub_key) as pub_key,
       (witness.hex_scale / 10000) as scale,
       witness.status,
@@ -47,13 +47,13 @@ SELECT
       witness.report.tmst as concentrator_tmst,
       (witness.report.signal / 10) as signal_dbm,
       witness.report.frequency as frequency_hz,
-      witness.report.snr,
+      (witness.report.snr / 10) as snr_db,
       witness.report.datarate,
       base64(witness.report.signature) as signature,
       (witness.reward_unit / 10000) as reward_unit,
       witness.invalid_reason,
-      witness.participant_side,
+      witness.participant_side as invalid_participant_side,
       (witness.gain / 10) as gain_dbi,
-      witness.elevation
+      witness.elevation as elevation_agl_m
     ) as witness    
 FROM raw
