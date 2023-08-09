3043f0767a76bbc9149a23a195d201de36684dcb
Michael Vierling <mvierling@gmail.com>
2023-08-08 23:15:57 -0700

    update publish

--- iot-cc-ranked/publish.sh
+++ iot-cc-ranked/publish.sh
@@ -2,5 +2,5 @@
 
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/k0m1p4t7
 docker build -t iot-cc-ranked .
-docker tag iot-cc-ranked:0.0.1 public.ecr.aws/k0m1p4t7/iot-cc-ranked:0.0.1
+docker tag iot-cc-ranked public.ecr.aws/k0m1p4t7/iot-cc-ranked:0.0.1
 docker push public.ecr.aws/k0m1p4t7/iot-cc-ranked:0.0.1
